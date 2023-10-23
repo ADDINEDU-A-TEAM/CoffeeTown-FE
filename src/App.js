4d9beecbbc2e7a3f7adbdd676c586ac50489c244
blackpeng15 <blackpeng15@naver.com>
2023-10-23 19:31:47 +0900

    'NoChange'

--- src/App.js
+++ src/App.js
@@ -11,78 +11,78 @@ import { Reset } from 'styled-reset';
 import { Routes, Route, useLocation } from 'react-router-dom';
 
 const App = () => {
-  const location = useLocation();
+    const location = useLocation();
 
-  const [data, setData] = useState([]);
-  const [user, setUser] = useState(location.state ? location.state?.user : '');
+    const [data, setData] = useState([]);
+    const [user, setUser] = useState(location.state ? location.state?.user : '');
 
-  console.log('appuser', location.state);
+    console.log('appuser', location.state);
 
-  const [cartItems, setCartItems] = useState([]);
+    const [cartItems, setCartItems] = useState([]);
 
-  const addToCart = (product) => {
-    const existingProductIndex = cartItems.findIndex(
-      (item) => item.product_id === product.product_id
+    const addToCart = (product) => {
+        const existingProductIndex = cartItems.findIndex(
+            (item) => item.product_id === product.product_id
+        );
+
+        if (existingProductIndex !== -1) {
+            // 이미 장바구니에 있는 상품의 수량 증가
+            const updatedCartItems = [...cartItems];
+            updatedCartItems[existingProductIndex] = {
+                ...updatedCartItems[existingProductIndex],
+                quantity: updatedCartItems[existingProductIndex].quantity + 1,
+            };
+            setCartItems(updatedCartItems);
+        } else {
+            // 장바구니에 없는 상품인 경우 수량 1로 추가
+            product.quantity = 1;
+            setCartItems([...cartItems, product]);
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        await axios
+            .get('http://localhost:3000/data/exData.json')
+            .then((result) => setData(result.data));
+    };
+
+    return (
+        <Fragment>
+            <div id='wrap' className='d-flex'>
+                <Reset />
+                <Header user={user} setUser={setUser} cartItems={cartItems} />
+                <Routes>
+                    <Route
+                        path={'/'}
+                        element={<Login user={user} setUser={setUser} />}
+                    ></Route>
+                    <Route path={'/Signup'} element={<Signup />}></Route>
+                    <Route
+                        path={'/Main'}
+                        element={
+                            <Main setData={setData} data={data} addToCart={addToCart} />
+                        }
+                    ></Route>
+                    <Route
+                        path={'/Cart'}
+                        element={
+                            <Cart
+                                setData={setData}
+                                data={data}
+                                cartItems={cartItems}
+                                setCartItems={setCartItems}
+                            />
+                        }
+                    ></Route>
+                </Routes>
+                <Footer />
+            </div>
+        </Fragment>
     );
-
-    if (existingProductIndex !== -1) {
-      // 이미 장바구니에 있는 상품의 수량 증가
-      const updatedCartItems = [...cartItems];
-      updatedCartItems[existingProductIndex] = {
-        ...updatedCartItems[existingProductIndex],
-        quantity: updatedCartItems[existingProductIndex].quantity + 1,
-      };
-      setCartItems(updatedCartItems);
-    } else {
-      // 장바구니에 없는 상품인 경우 수량 1로 추가
-      product.quantity = 1;
-      setCartItems([...cartItems, product]);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const fetchData = async () => {
-    await axios
-      .get('http://localhost:3000/data/exData.json')
-      .then((result) => setData(result.data));
-  };
-
-  return (
-    <Fragment>
-      <div id='wrap' className='d-flex'>
-        <Reset />
-        <Header user={user} setUser={setUser} cartItems={cartItems} />
-        <Routes>
-          <Route
-            path={'/'}
-            element={<Login user={user} setUser={setUser} />}
-          ></Route>
-          <Route path={'/Signup'} element={<Signup />}></Route>
-          <Route
-            path={'/Main'}
-            element={
-              <Main setData={setData} data={data} addToCart={addToCart} />
-            }
-          ></Route>
-          <Route
-            path={'/Cart'}
-            element={
-              <Cart
-                setData={setData}
-                data={data}
-                cartItems={cartItems}
-                setCartItems={setCartItems}
-              />
-            }
-          ></Route>
-        </Routes>
-        <Footer />
-      </div>
-    </Fragment>
-  );
 };
 
 export default App;
